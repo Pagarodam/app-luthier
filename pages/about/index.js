e8abbb0e4dbbcf05aabd6a1b704b3a3c9576b574
Pagarodam <pagarodam@gmail.com>
2022-03-11 17:39:05 +0100

    Registro con mail y contraseña, redireccionamiento a about despues de login.

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+
+export default function About(){
+return (
+    <main>
+        <h1>
+          Cositas de Guitarritas
+        </h1>
+    </main>
+    )
+}
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,14 +1,20 @@
 import Button from "../../components/Button";
 import { GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import { authentication } from "../../components/firebase/client";
+import { useRouter } from "next/router";
 
 
+//Google Login
 export default function Login() {
+
+  const { push } = useRouter();
+
   const signInWithGoogle = ()=> {
     const provider = new GoogleAuthProvider();
     signInWithPopup(authentication, provider)
     .then((res)=>{
       console.log(res);
+      push("/about");
     })
     .catch((err)=>{
       console.log(err);
@@ -17,3 +23,5 @@ export default function Login() {
 
   return <Button style="" onClick={signInWithGoogle} label='Login with Google'/>;
 }
+
+//Email Login
