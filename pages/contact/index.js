39a01741c557d6e72f24fcd5cefcff1abb2d5bc6
juanfril <juanfril@gmail.com>
2022-03-22 21:52:46 +0100

    fix nav repeating

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,60 +1,92 @@
 import Nav from '../../components/nav';
 import styles from '../../styles/Home.module.css';
 
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <div class='contact_form'>
+        <div class='formulario'>
+          <h1>Formulario de contacto</h1>
+          <h3>Escríbenos y en breve nos pondremos en contacto contigo</h3>
 
-export default function About(){
-    return (
-        <div className={styles.container}>
-            <Nav></Nav>
-            <div class="contact_form">
-                <div class="formulario">      
-                    <h1>Formulario de contacto</h1>
-                    <h3>Escríbenos y en breve nos pondremos en contacto contigo</h3>
+          <form>
+            <p>
+              <label>
+                Nombre
+                <span>*</span>
+                <input
+                  type='text'
+                  name='name'
+                  id='nombre'
+                  required='obligatorio'
+                  placeholder='Escribe tu nombre'
+                />
+              </label>
+            </p>
 
-                    <form>       
-                        <p>
-                            <label>Nombre
-                                <span>*</span>
-                                <input type="text" name="name" id="nombre" required="obligatorio" placeholder="Escribe tu nombre"/>
-                            </label>
-                        </p>
-                    
-                        <p>
-                            <label>Email
-                                <span>*</span>
-                                <input type="email" name="email" id="email" required="obligatorio" placeholder="Escribe tu Email"/>
-                            </label>
-                        </p>
-                    
-                        <p>
-                            <label>Teléfono
-                                <input type="tel" name="telefono" id="telefono" placeholder="Escribe tu teléfono"/>
-                            </label>
-                        </p>     
-                    
-                        <p>
-                            <label>Asunto
-                                <span>*</span>
-                                <input type="text" name="asunto" id="assunto" required="obligatorio" placeholder="Escribe un asunto"/>
-                            </label>
-                        </p>    
-                    
-                        <p>
-                            <label>Mensaje
-                                <span>*</span>
-                                <textarea name="mensaje" id="mensaje" required="obligatorio" placeholder="Deja aquí tu comentario..."></textarea> 
-                            </label>                     
-                        </p>                    
-                    
-                        <button type="submit" name="enviar" id="enviar"><p>Enviar</p></button>
+            <p>
+              <label>
+                Email
+                <span>*</span>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  required='obligatorio'
+                  placeholder='Escribe tu Email'
+                />
+              </label>
+            </p>
 
-                        <p>
-                            <span> * </span>los campos son obligatorios.
-                        </p>   
+            <p>
+              <label>
+                Teléfono
+                <input
+                  type='tel'
+                  name='telefono'
+                  id='telefono'
+                  placeholder='Escribe tu teléfono'
+                />
+              </label>
+            </p>
 
-                    </form>
-                </div>
-            </div>
+            <p>
+              <label>
+                Asunto
+                <span>*</span>
+                <input
+                  type='text'
+                  name='asunto'
+                  id='assunto'
+                  required='obligatorio'
+                  placeholder='Escribe un asunto'
+                />
+              </label>
+            </p>
+
+            <p>
+              <label>
+                Mensaje
+                <span>*</span>
+                <textarea
+                  name='mensaje'
+                  id='mensaje'
+                  required='obligatorio'
+                  placeholder='Deja aquí tu comentario...'
+                ></textarea>
+              </label>
+            </p>
+
+            <button type='submit' name='enviar' id='enviar'>
+              <p>Enviar</p>
+            </button>
+
+            <p>
+              <span> * </span>los campos son obligatorios.
+            </p>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
