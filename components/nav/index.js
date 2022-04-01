65015a34334e4697bf67a7b9e77c3a7eb0d7c2b4
juanfril <juanfril@gmail.com>
2022-04-01 22:29:53 +0200

    add tailwind dependencies nav modifity

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,69 +1,62 @@
-import styles from '../../styles/Home.module.css';
+import { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Button from '../Button';
-import { useAuth } from '../Auth';
 
-export default function Nav() {
-  const { user } = useAuth();
-  const { logOut } = useAuth();
-  console.log('current nav', user);
-
-  const floatRight = {
-    float: 'right',
-  };
-
-  const router = useRouter();
+const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
 
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.ul}>
-        <li className={router.pathname === '/' ? styles.active : ''}>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        {1 === 1 && (
-          <>
-            <li
-              className={router.pathname === '/register' ? styles.active : ''}
-            >
-              <Link href="/register">
-                <a>Registrate</a>
-              </Link>
-            </li>
-            <li className={router.pathname === '/login' ? styles.active : ''}>
-              <Link href="/login">
-                <a>Login</a>
-              </Link>
-            </li>
-          </>
-        )}
+    <header className="flex items-center p-3 flex-wrap text-white bg-blue-900">
+      <div
+        id="logo"
+        className="lg:text-xl p-2 mr-4 inline-flex items-center font-serif font-bold"
+      >
+        <Link href="/">
+          <a>NextJS with Tailwind</a>
+        </Link>
+      </div>
+      <button
+        onClick={() => setShowNav(!showNav)}
+        type="button"
+        className="inline-flex p-3 text-white hover:text-gray-300 focus:text-white focus:outline-none lg:hidden ml-auto"
+      >
+        <svg
+          className="h-6 w-6 fill-current"
+          viewBox="0 -53 384 384"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+          <path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+          <path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+        </svg>
+      </button>
 
-        <li className={router.pathname === '/gallery' ? styles.active : ''}>
-          <Link href="/gallery">
-            <a>Galeria</a>
+      <div className="w-full flex-grow lg:inline-flex lg:flex-grow lg:w-auto">
+        <div
+          className={
+            'lg:inline-flex lg:flex-row lg:ml-auto flex flex-col ' +
+            (showNav ? '' : 'hidden')
+          }
+        >
+          <Link href="/gallery/woods">
+            <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded hover:bg-gray-900">
+              Maderas
+            </a>
           </Link>
-        </li>
-        {1 === 1 && (
-          <>
-            <li className={styles.active}>{user ? user.email : 'logeate'}</li>
-            <li>
-              <Button style="" onClick={logOut} label="Sign Out" />
-            </li>
-          </>
-        )}
-        <li className={router.pathname === '/contact' ? styles.active : ''}>
-          <Link href="/contact">
-            <a>Contacto</a>
-          </Link>
-        </li>
-        <li className={router.pathname === '/about' ? styles.active : ''}>
+
           <Link href="/about">
-            <a>Acerca de</a>
+            <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded  hover:bg-gray-900">
+              About
+            </a>
           </Link>
-        </li>
-      </ul>
-    </nav>
+          <Link href="/contact">
+            <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded hover:bg-gray-900">
+              Contact Us
+            </a>
+          </Link>
+        </div>
+      </div>
+    </header>
   );
-}
+};
+
+export default Navbar;
