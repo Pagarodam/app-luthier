ed70e6ce2fe0800686747a54115f156ea528048b
Pagarodam <pagarodam@gmail.com>
2022-05-22 21:08:25 +0200

    Filtradas guitarras por clasicas o flamencas en shop

--- components/guitars/GuitarList.js
+++ components/guitars/GuitarList.js
@@ -2,15 +2,8 @@ import Titles from 'components/UI/Titles';
 import { GuitarCard } from './GuitarCard';
 
 const GuitarList = ({ guitars, deleteGuitar, label }) => {
-  // const getGuitarsByStyle = (guitars, guitarStyle) =>
-  //   guitars.filter((guitar) => guitar.style === guitarStyle);
-
-  // const clasic = getGuitarsByStyle(guitars, 'clasico');
-  // const flamenco = getGuitarsByStyle(guitars, 'flamenco');
-
   return (
     <>
-      {/* {guitars?.length || (guitars?.length && <Titles label={'Guitarras'} />)} */}
       {guitars?.length && <Titles label={label} />}
       <div className="flex flex-wrap justify-around">
         {guitars?.map((guitar) => (
