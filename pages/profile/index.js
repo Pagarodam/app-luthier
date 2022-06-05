9b4fb390e2c42255f2e759fd007ae00d239e235d
juanfril <juanfril@gmail.com>
2022-06-05 17:15:57 +0200

    profile style

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -73,19 +73,48 @@ const Profile = () => {
 
   return (
     <>
-      <h1>{JSON.stringify(address)}</h1>
+      <section className="relative py-16 bg-gray-300 mx-10 rounded">
+        <div className="container mx-auto px-4">
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-30">
+            <div className="px-6">
+              <div className="flex flex-wrap justify-center">
+                <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
+                  <div className="relative">
+                    <Image
+                      src={session.user.image}
+                      width={100}
+                      height={100}
+                      alt="profile"
+                      className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                      objectFit="cover"
+                    />
+                  </div>
+                </div>
+                <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"></div>
+                <div className="w-full lg:w-4/12 px-4 lg:order-1"></div>
+              </div>
+              <div className="text-center mt-12">
+                <h3 className="text-4xl font-semibold leading-normal text-gray-800 mb-2">
+                  {session.user.name}
+                </h3>
+                <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
+                  <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{' '}
+                  {session.user.address.city}
+                </div>
+                <div className="mb-2 text-gray-700 mt-10">
+                  <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
+                  {session.user.address.street}
+                </div>
+                <div className="mb-2 text-gray-700">
+                  <i className="fas fa-university mr-2 text-lg text-gray-500"></i>
+                  {session.user.address.cp}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <div className="mx-10">
-        <Image
-          src={session.user.image}
-          width={100}
-          height={100}
-          alt="profile"
-          className="rounded-full m-4"
-          objectFit="cover"
-        />
-        <h2 className="mx-auto">{session.user.name}</h2>
-        <p className="">{session.user.email}</p>
-        <p>{JSON.stringify(session.user.address)}</p>
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col">
             <Input
