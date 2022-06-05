7eca023a0aa43aa0e3dcdde0b89563b8a1db7a90
juanfril <juanfril@gmail.com>
2022-06-05 16:26:16 +0200

    login page

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,5 +1,6 @@
 import { useSession, getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
 
 export default function Login() {
   const { data: session } = useSession();
@@ -9,12 +10,18 @@ export default function Login() {
 
   return (
     <>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => signIn('google', { callbackUrl: '/' })}
-      >
-        Login
-      </button>
+      <div className="grid place-items-center h-48">
+        <h1 className="text-center m-4 underline decoration-sky-500 mt-4 antialiased text-3xl">
+          LOGIN
+        </h1>
+        <button
+          className="px-10 py-2 text-base text-white bg-blue-500 rounded shadow focus:outline-none hover:bg-blue-400"
+          onClick={() => signIn('google', { callbackUrl: '/' })}
+        >
+          <FcGoogle />
+          Sign In with Google
+        </button>
+      </div>
     </>
   );
 }
